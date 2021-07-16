7e9d20dddabdf7decdcaefdcfce451bf82422240
jbrainz <developer@roberronero.com>
2021-07-16 18:53:10 +0100

    wrapper component

--- src/app/containers/home/index.tsx
+++ src/app/containers/home/index.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components"
 import tw from "twin.macro"
+import BookCarCard from "../../components/bookCard/BookCar"
+import Marginer from "../../components/marginer/Marginer"
 import Navbar from "../../components/navbar/Navbar"
+import BookingSteps from "./BookingSteps"
+import HomeAbout from "./HomeAbout"
 import TopSections from "./TopSections"
 
 const PageContainer = styled.div`
@@ -18,6 +22,12 @@ const Homepage: React.FC = () => {
     <PageContainer>
       <Navbar />
       <TopSections/>
+      <Marginer direction="vertical" margin="4em"/>
+      <BookCarCard/>
+      <Marginer direction="vertical" margin="8em"/>
+      <BookingSteps/>
+      <Marginer direction="vertical" margin="8em"/>
+      <HomeAbout/>
     </PageContainer>
   )
 }
