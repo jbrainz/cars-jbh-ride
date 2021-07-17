567b7ed196bcdea3fc36f01d3b034145d83bbdea
jbrainz <developer@roberronero.com>
2021-07-18 00:49:35 +0100

    some changes to the general app wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import styled from "styled-components"
 import tw from "twin.macro"
+import {Footer, Navbar} from "./app/layouts";
 import HomePage from './app/containers/home/index'
 
 const AppContainer = styled.div`
 ${tw`
   w-full
-  h-full
   flex
   flex-col
+  overflow-x-hidden
+  items-center
 `}
 `;
 
 function App() {
   return <AppContainer>
+    <Navbar/>
     <HomePage/>
+    <Footer/>
   </AppContainer>
 }
 
