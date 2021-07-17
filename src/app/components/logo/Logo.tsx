11da3ae24e6a57473092a2f3f2762fd57cb94c71
jbrainz <developer@roberronero.com>
2021-07-18 00:50:56 +0100

    support for custom tweaking on the logo component

--- src/app/components/logo/Logo.tsx
+++ src/app/components/logo/Logo.tsx
@@ -1,7 +1,10 @@
-import styled from "styled-components"
-import tw from "twin.macro"
-import CarLogoImage from "../../../assets/logo/jbh.jpeg"
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import CarLogoImage from '../../../assets/logo/jbh.jpeg'
 
+interface ILogoProps {
+  color?: 'white' | 'dark'
+}
 const LogoContainer = styled.div`
   ${tw`
 flex
@@ -16,29 +19,30 @@ const LogoText = styled.div`
   font-bold
   text-black
   m-1
-`}
-`
+  `}
+  ${({ color }: any) => (color === 'white' ? tw`text-white` : tw`text-black`)}
+` as any
 
 const Image = styled.div`
   width: auto;
+  border-radius: 50%;
   ${tw`
-  h-6
-  md:h-9
   `}
-
+  height: 4.5rem;
   img {
     width: auto;
     height: 100%;
+    border-radius: 50%;
   }
 `
 
-const Logo = () => {
+const Logo = ({ color }: ILogoProps) => {
   return (
     <LogoContainer>
       <Image>
-        <img src={CarLogoImage} alt="jbh" />
+        <img src={CarLogoImage} alt='jbh' />
       </Image>
-      <LogoText>JBH Global</LogoText>
+      <LogoText color={color || 'dark'}>JBH Global</LogoText>
     </LogoContainer>
   )
 }
